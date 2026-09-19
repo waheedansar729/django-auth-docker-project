3767eccda0069292430fd295a146ea31f6b00304
waheedansar29 <waheedansar29@gmail.com>
2026-09-20 01:12:28 +0500

    feat: complete django custom auth, api endpoints, and tests

--- core/settings.py
+++ core/settings.py
@@ -134,3 +134,13 @@ MAILERS = {
 }
 
 AUTH_USER_MODEL = 'api.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
